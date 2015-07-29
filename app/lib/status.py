eb535f26c7747bbeee826162fbbcb812fce3a770
Charlie Fierro <charlie@inkling.com>
2015-07-29 10:36:34 -0700

    Add custom error handling

---
+++ app/lib/status.py
@@ -0,0 +1,23 @@
+
+class TodoException(Exception):
+    """Base exception that all errors should inherit from.
+    """
+    statusCode = 500
+
+    def __init__(self, message, details=None, result=None, info=None):
+        super(BaseException, self).__init__()
+        self.statusMsg = message
+        self.statusDetails = details or {}
+        self.result = result or {}
+        self.info = info or {}
+
+    def toDict(self):
+        return {
+            'result': self.result,
+            'info': self.info,
+            'status': {
+                'statusCode': self.statusCode,
+                'statusMsg': self.statusMsg,
+                'statusDetails': self.statusDetails,
+            },
+        }
