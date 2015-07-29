952661d675a93b979ba46db441864fb6b06485b3
Charlie Fierro <charlie@inkling.com>
2015-07-29 10:50:28 -0700

    Update user table to be more rigorous and fix todos table

--- app/users/models.py
+++ app/users/models.py
@@ -2,9 +2,9 @@ from app import db
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    email = db.Column(db.String, unique=True)
-    password = db.Column(db.String)
+    name = db.Column(db.String, nullable=False)
+    email = db.Column(db.String, unique=True, nullable=False)
+    password = db.Column(db.String, nullable=False)
 
     def __init__(self, name, email, password):
         self.name = name
